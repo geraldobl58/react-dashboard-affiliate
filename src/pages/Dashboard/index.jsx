f52fe08463c8b48c6c63bfa56b6944caed1c7bf4
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-10 09:25:28 -0300

    :sparkles: Creating dashboard page

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Appshell from '../../components/Appshell';
+
+const Dashboard = () => (
+  <>
+    <Appshell>Dashboard</Appshell>
+  </>
+);
+
+export default Dashboard;
