ec34d51a17497183605c72b2f8e701c14480237f
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-11 10:54:17 -0300

    :sparkles: Creating hook loading

--- src/index.jsx
+++ src/index.jsx
@@ -6,13 +6,17 @@ import { ThemeProvider } from '@material-ui/core/styles';
 
 import App from './App';
 import theme from './theme';
+
 import MessageProvider from './hooks/Messages';
+import LoadingProvider from './hooks/Loading';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <MessageProvider>
-      <CssBaseline />
-      <App />
+      <LoadingProvider>
+        <CssBaseline />
+        <App />
+      </LoadingProvider>
     </MessageProvider>
   </ThemeProvider>,
   document.getElementById('root')
