bdfb58912fdbf314a53bcbf62ca3e156c3797abf
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-12 17:31:17 -0300

    :hammer: Helper for dates

--- src/utils/dateFormatted.js
+++ src/utils/dateFormatted.js
@@ -1,9 +1,10 @@
 function dateFormatted(date) {
-  return new Intl.DateTimeFormat('pt-BR', {
-    day: 'numeric',
-    month: 'numeric',
-    year: 'numeric',
-  }).format(new Date(date));
+  const day = date.split('-')[0];
+  const month = date.split('-')[1];
+  const year = date.split('-')[2];
+
+  return `${year}/${`0${month}`.slice(-2)}/${`0${day}`.slice(-2)}`;
+  // Utilizo o .slice(-2) para garantir o formato com 2 digitos.
 }
 
 export default dateFormatted;
