f2c6a4d3cd8b82d181373dcbbdbb1ea57143988a
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-16 12:18:18 -0300

    :hammer: Refactoring payments page

--- src/template/PaymentsCard/index.jsx
+++ src/template/PaymentsCard/index.jsx
@@ -10,56 +10,54 @@ import { ContainerCard } from './styles';
 const PaymentsCard = ({ paymentSearch }) => {
   return (
     <Grid>
-      <Grid container spacing={3}>
-        {paymentSearch.map((item) => (
-          <>
-            <Grid item xs={12}>
-              <Typography variant="h5" component="h2">
-                {wordUpper(item.bandeira)}
+      {paymentSearch.map((item) => (
+        <Grid container spacing={3} key={item.id}>
+          <Grid item xs={12}>
+            <Typography variant="h5" component="h2">
+              {wordUpper(item.bandeira)}
+            </Typography>
+          </Grid>
+          <ContainerCard>
+            <Grid item xs={4}>
+              <Typography variant="body2" component="p">
+                Valor Venda Total
               </Typography>
+              <Typography variant="h6">{item.valorVendaTotal}</Typography>
             </Grid>
-            <ContainerCard>
-              <Grid item xs={4}>
-                <Typography variant="body2" component="p">
-                  Valor Venda Total
-                </Typography>
-                <Typography variant="h6">{item.valorVendaTotal}</Typography>
-              </Grid>
-              <Grid item xs={4}>
-                <Typography variant="body2" component="p">
-                  Valor Venda Aprovado
-                </Typography>
-                <Typography variant="h6">{item.valorVendaAprovado}</Typography>
-              </Grid>
-              <Grid item xs={4}>
-                <Typography variant="body2" component="p">
-                  Investimento
-                </Typography>
-                <Typography variant="h6">{item.investimento}</Typography>
-              </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" component="p">
+                Valor Venda Aprovado
+              </Typography>
+              <Typography variant="h6">{item.valorVendaAprovado}</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" component="p">
+                Investimento
+              </Typography>
+              <Typography variant="h6">{item.investimento}</Typography>
+            </Grid>
 
-              <Grid item xs={4}>
-                <Typography variant="body2" component="p">
-                  Aprovado
-                </Typography>
-                <Typography variant="h6">{item.aprovado}</Typography>
-              </Grid>
-              <Grid item xs={4}>
-                <Typography variant="body2" component="p">
-                  ROAS
-                </Typography>
-                <Typography variant="h6">{item.roas}</Typography>
-              </Grid>
-              <Grid item xs={4}>
-                <Typography variant="body2" component="p">
-                  Visitas
-                </Typography>
-                <Typography variant="h6">{item.visitas}</Typography>
-              </Grid>
-            </ContainerCard>
-          </>
-        ))}
-      </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" component="p">
+                Aprovado
+              </Typography>
+              <Typography variant="h6">{item.aprovado}</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" component="p">
+                ROAS
+              </Typography>
+              <Typography variant="h6">{item.roas}</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="body2" component="p">
+                Visitas
+              </Typography>
+              <Typography variant="h6">{item.visitas}</Typography>
+            </Grid>
+          </ContainerCard>
+        </Grid>
+      ))}
     </Grid>
   );
 };
